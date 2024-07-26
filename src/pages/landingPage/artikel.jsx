08d1f054b15b9a06b881cae7c8e593d6178579d0
Ardiyansyah R <arsk9797@gmail.com>
2024-07-26 23:19:08 +0800

    add key card pada artikel.jsx

--- src/pages/landingPage/artikel.jsx
+++ src/pages/landingPage/artikel.jsx
@@ -7,52 +7,51 @@ import CardArtikel from "../../components/Fragments/Cards/CardArtikel";
 import CardArtikelHome from "../../components/Fragments/Cards/HomeCardArtikel";
 
 export default function ArtikelPage() {
-    const [articles, setArticles] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        setLoading(true);
-        let fecthData = async () => {
-            try {
-                const dataArticle = await getAllArticles();
-                setArticles(dataArticle);
-            } catch (err) {
-                console.log(err);
-            } finally {
-                setLoading(false);
-            }
-        };
-        fecthData();
-    }, []);
-    return (
-        <>
-            <HeroSection className="h-80 md:h-96">
-                <div className="text-center text-white">
-                    <h2 className="text-xl font-semibold md:text-2xl max-w-xl mb-5">Artikel & Berita</h2>
-                    <TextBlink className={`text-base max-w-sm`}>Telaah Artikel menarik di Negeri 1000 Megalit Sulawesi Tengah</TextBlink>
-                </div>
-            </HeroSection>
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    let fecthData = async () => {
+      try {
+        const dataArticle = await getAllArticles();
+        setArticles(dataArticle);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fecthData();
+  }, []);
+  return (
+    <>
+      <HeroSection className="h-80 md:h-96">
+        <div className="text-center text-white">
+          <h2 className="mb-5 max-w-xl text-xl font-semibold md:text-2xl">
+            Artikel & Berita
+          </h2>
+          <TextBlink className={`max-w-sm text-base`}>
+            Telaah Artikel menarik di Negeri 1000 Megalit Sulawesi Tengah
+          </TextBlink>
+        </div>
+      </HeroSection>
 
-            {
-                loading ? (<Loading />) : (
-                    <div className="my-12 flex flex-wrap px-10 gap-5 justify-center md:px-0 md:justify-evenly lg:justify-center">
-                        {
-                            articles.length > 0 && articles.map(article => (
-                                <>
-                                    <CardArtikel
-                                        title={article.title}
-                                        to={`/artikel/${article.id}`}
-
-                                    />
-
-                                </>
-                            ))
-                        }
-                    </div>
-                )
-            }
-        </>
-    );
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="my-12 flex flex-wrap justify-center gap-5 px-10 md:justify-evenly md:px-0 lg:justify-center">
+          {articles.length > 0 &&
+            articles.map((article) => (
+              <>
+                <CardArtikel
+                  key={article.id}
+                  title={article.title}
+                  to={`/artikel/${article.id}`}
+                />
+              </>
+            ))}
+        </div>
+      )}
+    </>
+  );
 }
-
-
-
